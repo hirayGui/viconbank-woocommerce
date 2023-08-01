a3ee2c62065fcf75a370f73bf2c6c1eb89ba8ca5
hiraygui <hiraygui@gmail.com>
2023-08-01 14:11:31 -0300

    criando arquivo inicial e definindo base do projeto

---
+++ viconbank-woocommerce.php
@@ -0,0 +1,11 @@
+<?php 
+
+/**
+ * Plugin Name: ViconBank Pagamentos
+ * Plugin URI: https://github.com/hiraygui/viconbank-woocommerce
+ * Description: Este plugin permite que sua loja virtual possa aceitar pagamentos através de Pix e Boleto
+ * Version: 0.1.0
+ * Author: Gizo Digital
+ * Author URI: 
+ * Text Domain: viconbank-woocommerce
+ */
