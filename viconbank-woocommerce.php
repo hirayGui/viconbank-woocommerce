db66c0fd4704a8e79d4715fdea6393aaff707d78
hiraygui <hiraygui@gmail.com>
2023-08-18 15:59:41 -0300

    corrigindo documentação do código

--- viconbank-woocommerce.php
+++ viconbank-woocommerce.php
@@ -5,13 +5,13 @@
  * Plugin URI:  https://github.com/hiraygui/viconbank-woocommerce
  * Author: Gizo Digital
  * Author URI: 
- * Description: Este plugin permite que sua loja virtual possa aceitar pagamentos através de Pix e Boleto
- * Version: 0.1.1
+ * Description: Este plugin permite que sua loja virtual possa aceitar pagamentos através de Pix utilizando sua conta ViconBank
+ * Version: 0.1.2
  * License:     GPL-2.0+
  * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: viconbank-woocommerce
  * 
- * Class WC_Gateway_ViconBank file.
+ * Class WC_ViconBank_Gateway file.
  *
  * @package WooCommerce\viconbank-woocommerce
  */
@@ -19,17 +19,6 @@
 if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
-
-/**
- * ViconBank Pagamentos Gateway.
- *
- * Provides a ViconBank Pagamentos Payment Gateway.
- *
- * @class       WC_ViconBank_Gateway
- * @extends     WC_Payment_Gateway
- * @version     0.1.1
- * @package     WooCommerce\Classes\Payment
- */
 
 //condição verifica se plugin woocommerce está ativo
 if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) return;
